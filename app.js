3a9ffec1b82e7e385124c5e6cf6e629687a6c439
Shruti Jain <jainshruti.rattan@gmail.com>
2023-02-10 14:48:52 +0530

    up

---
+++ app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded',()=>{
+
+    const grid = document.querySelector('.grid')
+    let squares = document.querySelectorAll('.grid div')
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector(#start-button)
+    cont width =10
+
+
+})
